bd7f9012197ad409a9adfb3b3e3006f8a46024ec
LA <no>
2024-07-07 16:46:24 +0500

    setup ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
 <x-layouts.app>
-    <h1> Testing </h1>
+    <header class="max-w-6xl mx-auto bg-green-200 flex justify-between">
+        <h1> LOGO </h1>
+        <nav>
+            <a href="#"> Home </a>
+            <a href="#"> About </a>
+            <a href="#"> Contact </a>
+        </nav>
+    </header>
+    <main class="max-w-6xl mx-auto bg-indigo-400 h-[80vh]">
+        <h1> Testing </h1>
+    </main>
+
+    <footer class="max-w-6xl h-72 mx-auto bg-red-200 flex justify-between">
+        <h1> Footer </h1>
+    </footer>
 </x-layouts.app>
